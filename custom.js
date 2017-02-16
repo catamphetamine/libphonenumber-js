fab0b19e39eeba3e4ced12bb8ae373858883240f
Nikolay <halt.hammerzeit.at@gmail.com>
2017-02-16 10:08:33 +0300

    fix get_number_type

--- custom.js
+++ custom.js
@@ -4,7 +4,7 @@ exports = module.exports = {}
 
 exports.parse             = require('./build/parse').default
 exports.format            = require('./build/format').default
-exports.get_number_type   = require('./build/format').get_number_type
+exports.get_number_type   = require('./build/parse').get_number_type
 exports.is_valid_number   = require('./build/validate').default
 exports.as_you_type       = require('./build/as you type').default
 exports.DIGIT_PLACEHOLDER = require('./build/as you type').DIGIT_PLACEHOLDER
