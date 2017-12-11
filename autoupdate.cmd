2300e132a3e6b66c35082f696a87f76ec70eff3d
catamphetamine <purecatamphetamine@gmail.com>
2017-12-11 22:09:08 +0300

    Windows cmd comments

--- autoupdate.cmd
+++ autoupdate.cmd
@@ -1,6 +1,6 @@
-# Set the current folder to the script's folder
+:: Set the current folder to the script's folder
 pushd %~dp0
-# Update metadata and release the new version
+:: Update metadata and release the new version
 npm run metadata:update:job
-# Restore the previous current folder
+:: Restore the previous current folder
 popd
