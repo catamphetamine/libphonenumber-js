162d880a4d14ca8762aa3501d45908af8d14d364
catamphetamine <purecatamphetamine@gmail.com>
2018-01-22 13:31:03 +0300

    Normalize numerical `value` note

--- source/types.js
+++ source/types.js
@@ -151,6 +151,9 @@ export function sort_out_arguments(arg_1, arg_2, arg_3)
 	// Normalize numerical `value`.
 	// https://github.com/catamphetamine/libphonenumber-js/issues/142
 	// `getNumberType(88005553535, ...)`.
+	// This is for legacy databases only
+	// because integers must not be used to represent phone numbers.
+	// https://github.com/googlei18n/libphonenumber/blob/master/FALSEHOODS.md
 	if (typeof arg_1 === 'number')
 	{
 		arg_1 = String(arg_1)
