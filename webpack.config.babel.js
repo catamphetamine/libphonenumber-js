79b9fe6f0d39c1187ba86d4c7a1f5cf3793c6205
catamphetamine <purecatamphetamine@gmail.com>
2018-03-21 08:44:30 +0300

    Webpack build update

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,45 +1,24 @@
 import webpack from 'webpack'
 import path from 'path'
 
-const env = process.env.WEBPACK_ENV
-
 const library_name = 'libphonenumber-js'
 const global_variable_name = 'libphonenumber'
 
-let output_file
-
-const plugins = []
-
-if (env === 'build')
-{
-  plugins.push(new webpack.optimize.UglifyJsPlugin
-  ({
-    minimize  : true,
-    sourceMap : true
-  }))
-
-  output_file = `${library_name}.min.js`
-}
-else
-{
-  output_file = `${library_name}.js`
-}
-
-const config =
+export default
 {
   entry: path.join(__dirname, '/index.es6.js'),
   devtool: 'source-map',
   output:
   {
     path           : path.join(__dirname, '/bundle'),
-    filename       : output_file,
+    filename       : `${library_name}.min.js`,
     library        : global_variable_name,
     libraryTarget  : 'umd',
     umdNamedDefine : true
   },
   module:
   {
-    loaders:
+    rules:
     [{
       test    : /(\.js)$/,
       loader  : 'babel-loader',
@@ -51,8 +30,5 @@ const config =
     // Use external version of React
     "react"     : "React",
     "react-dom" : "ReactDOM"
-  },
-  plugins
+  }
 }
-
-module.exports = config
