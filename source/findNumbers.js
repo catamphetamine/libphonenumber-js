ad2dc942dda4d48c02923bab2fba5c90e0731fe5
catamphetamine <purecatamphetamine@gmail.com>
2018-04-16 00:23:55 +0300

    MAX_SAFE_INTEGER

--- source/findNumbers.js
+++ source/findNumbers.js
@@ -1,12 +1,14 @@
 import { sort_out_arguments } from './findPhoneNumbers'
 import PhoneNumberMatcher from './PhoneNumberMatcher'
 
+const MAX_SAFE_INTEGER = Number.MAX_SAFE_INTEGER || Math.pow(2, 53) - 1
+
 export default function findNumbers(arg_1, arg_2, arg_3, arg_4)
 {
 	const { text, options, metadata } = sort_out_arguments(arg_1, arg_2, arg_3, arg_4)
 
 	options.leniency = options.leniency || 'VALID'
-	options.maxTries = options.maxTries || Number.MAX_SAFE_INTEGER
+	options.maxTries = options.maxTries || MAX_SAFE_INTEGER
 
 	// See `.hasNext()` and `.next()` methods of `PhoneNumberMatcher`.
 	return new PhoneNumberMatcher
