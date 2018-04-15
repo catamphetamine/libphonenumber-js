fa742e74c3593312e84788ea250fb1672dc9e154
catamphetamine <purecatamphetamine@gmail.com>
2018-04-16 00:28:05 +0300

    autoupdate npm install

--- autoupdate.cmd
+++ autoupdate.cmd
@@ -1,7 +1,5 @@
 :: Set the current folder to the script's folder.
 pushd %~dp0
-:: Update dependencies (so that it doesn't throw "module not found").
-npm install
 :: Update metadata and release the new version.
 npm run metadata:update:job
 :: Restore the previous current folder.
