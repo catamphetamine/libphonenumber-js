77d623c464e1570eb4c090566965d1b638a0d4b6
JinHoSo <31269303+JinHoSo@users.noreply.github.com>
2018-11-06 16:21:26 +0900

    Update index.d.ts
    fixed type error of parseIncompletePhoneNumber

--- index.d.ts
+++ index.d.ts
@@ -100,6 +100,7 @@ export function getPhoneCode(countryCode: CountryCode): CountryCallingCode;
 export function formatIncompleteParsedNumber(number: string, countryCode?: CountryCode): string;
 export function parseIncompleteParsedNumber(text: string): string;
 export function parseParsedNumberCharacter(character: string): string;
+export function parseIncompletePhoneNumber(character: string): string;
 
 export class AsYouType {
   constructor(defaultCountryCode?: CountryCode);
