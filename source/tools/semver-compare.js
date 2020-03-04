8792c9b054896c2ce493fdcd612dcb9e24429e93
catamphetamine <purecatamphetamine@gmail.com>
2020-03-04 22:12:16 +0300

    #381. Fixed semver-compare

--- source/tools/semver-compare.js
+++ source/tools/semver-compare.js
@@ -3,16 +3,27 @@
 //
 // Inlining this function because some users reported issues with
 // importing from `semver-compare` in a browser with ES6 "native" modules.
-export default function cmp (a, b) {
-    var pa = a.split('.');
-    var pb = b.split('.');
+//
+// Fixes `semver-compare` not being able to compare versions with alpha/beta/etc "tags".
+// https://github.com/catamphetamine/libphonenumber-js/issues/381
+export default function(a, b) {
+    // split by pre-releases
+    a = a.split('-')
+    b = b.split('-')
+    a[1] = a[1] || ''
+    b[1] = b[1] || ''
+    var pa = a[0].split('.')
+    var pb = b[0].split('.')
     for (var i = 0; i < 3; i++) {
-        var na = Number(pa[i]);
-        var nb = Number(pb[i]);
-        if (na > nb) return 1;
-        if (nb > na) return -1;
-        if (!isNaN(na) && isNaN(nb)) return 1;
-        if (isNaN(na) && !isNaN(nb)) return -1;
+        var na = Number(pa[i])
+        var nb = Number(pb[i])
+        if (na > nb) return 1
+        if (nb > na) return -1
+        if (!isNaN(na) && isNaN(nb)) return 1
+        if (isNaN(na) && !isNaN(nb)) return -1
     }
-    return 0;
-};
+    if (a[1] && b[1]) {
+        return a[1] > b[1] ? 1 : (a[1] < b[1] ? -1 : 0)
+    }
+    return !a[1] && b[1] ? 1 : (a[1] && !b[1] ? -1 : 0)
+}
